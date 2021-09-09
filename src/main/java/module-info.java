4ef0131152f9c5c1193f8747aba6d2f5f48a02c1
lucalewin <luca.lewin23@gmail.com>
2021-09-09 21:03:03 +0200

    update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,10 @@
-module net.lucraft.converter {
+module lucraft.converter {
 	requires javafx.controls;
 	requires javafx.fxml;
 	requires javafx.web;
+	requires javafx.swing;
+
+	requires java.desktop;
 
 	requires org.controlsfx.controls;
 	requires com.dlsc.formsfx;
@@ -9,7 +12,15 @@ module net.lucraft.converter {
 	requires org.kordamp.ikonli.javafx;
 	requires org.kordamp.bootstrapfx.core;
 	requires eu.hansolo.tilesfx;
+	requires kernel;
+	requires layout;
+	requires io;
+	requires annotations;
+
+	requires org.apache.commons.io;
 
 	opens net.lucraft.converter to javafx.fxml;
-	exports net.lucraft.converter;
+	opens net.lucraft.converter.ui to javafx.fxml;
+
+	exports net.lucraft.converter to javafx.graphics;
 }
